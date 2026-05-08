ff30c3693415d9d8aeaeada20ad1bde2fb2f0f05
RottenOtter14111 <arogers@anamosa.k12.ia.us>
2026-05-07 21:30:47 -0500

    work?

--- Organization.cs
+++ Organization.cs
@@ -1,19 +1,14 @@
-namespace knightmoves;
+﻿namespace knightmoves;
 
-public class Organization
-{
-    private string Name;
-    private int YearEstablished = 1934;
+public class Organization{
+   private string Name;
+   private int YearEstablished = 1934;
 
-    public Organization(string name)
-    {
-        Name = name;
-    }
+   public Organization(string name){
+      Name = name;
+   }
 
-    public string LookUpBranding(bool includeYearEstablished)
-    {
-        return includeYearEstablished 
-               ? $"{Name} est. {YearEstablished}" 
-               : Name;
-    }
+   public string LookUpBranding(bool includeYearEstablished){
+      return includeYearEstablished ? $"{Name} est. {YearEstablished}" : Name;
+   }
 }
